5509ebe0014ecdac7d691e01debe5daa3c961a15
Samuel-Berin <berin.s@husky.neu.edu>
2018-09-25 16:08:19 -0400

    Timeout working as expected; color difference

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 export default function game_init(root) {
-  ReactDOM.render(<MatchGame/>, root);
+  ReactDOM.render(<MatchGame />, root);
 }
 
 
@@ -16,8 +16,14 @@ export default function game_init(root) {
     //Each square is a tuple of [LETTER, IS REVEALED]
     // etc
 
-    //activeGuess: string
-    // The last letter guessed. If "", then no active guess
+    //firstGuess: string
+    // First guess in a pair. If "", then no guess
+
+    //secondGuess: string
+    // Second guess in a pair. If "", then no guess
+
+    //Score: int
+    // Number of times clicked on the page
 // }
 
 
@@ -25,6 +31,17 @@ export default function game_init(root) {
 class MatchGame extends React.Component {
   constructor(props) {
     super(props);
+    var boardModel = this.rollBoard();
+
+    this.state = {
+      board: boardModel,
+      firstGuess: "",
+      secondGuess: "",
+      score: 0,
+    };}
+
+
+  rollBoard() {
     var availableLetters = ["A", "A", "B", "B", "C", "C",
      "D","D", "E", "E", "F", "F", "G", "G", "H", "H"];
     var boardModel = [];
@@ -38,68 +55,97 @@ class MatchGame extends React.Component {
       }
       boardModel.push(row);
     }
+    return boardModel;
+  }
 
-    this.state = {
-      board: boardModel,
-      activeGuess: "",
-    };}
+  restart() {
+    var newBoard = this.rollBoard();
+    var state1 = {
+      board: newBoard,
+      firstGuess: "",
+      secondGuess: "",
+      score: 0,
+    }
+    this.setState(state1);
+  }
 
 
   guess(row, col) {
-    if (this.state.activeGuess == "") {
+
+    if (this.state.firstGuess != "" && this.state.secondGuess != "") {
+      //Do Nothing
+      //We're waiting on a timeout
+    }
+
+    else if (this.state.firstGuess == "") {
+      var newScore = this.state.score + 1;
+      var letter = this.state.board[row][col][0];
+      var state1 = Object.assign(this.state, {
+        firstGuess: letter,
+        score: newScore,
+      } );
+      this.setState(state1);
       this.revealByIndices(row, col);
     }
 
-    // There is already an active guess, compare
+    // There is already an first guess, compare
     else {
       var newGuess = this.state.board[row][col];
       var letter = newGuess[0];
-      if (letter == this.state.activeGuess) {
-        this.reveal(letter);
-      }
+      var newScore = this.state.score + 1;
+      var state1 = Object.assign(this.state, {
+        secondGuess: letter,
+        score: newScore,
+      } );
+      this.setState(state1);
 
-      else {
-        this.conceal(this.state.activeGuess)
-        this.conceal(letter);
-      }
+      this.revealByIndices(row, col);
+      setTimeout(function () {
+        this.updateConcealedValues();
+      }.bind(this), 1000);
+
     }
   }
 
   revealByIndices(row, col) {
     var guessLetter = this.state.board[row][col][0];
     this.state.board[row][col] = [guessLetter, true];
+    var newScore = this.state.score + 1;
     var state1 = Object.assign(this.state, {
       board: this.state.board,
-      activeGuess: guessLetter,
     } );
     this.setState(state1);
   }
 
 
 
-  reveal(letter) {
-    this.shouldReveal(letter, true);
-  }
 
-  conceal(letter) {
-    this.shouldReveal(letter, false);
-  }
-
-
-  shouldReveal(letter, bool) {
+  updateConcealedValues() {
     var newboard = this.state.board.slice(0)
+    var correctGuess = this.state.firstGuess == this.state.secondGuess;
+    console.log(this.state.firstGuess);
+    console.log(this.state.secondGuess);
     for (var i = 0; i < 4; i++) {
       for (var j = 0; j < 4; j++) {
-        var letter2 = this.state.board[i][j][0]
-        if (letter == letter2) {
-          newboard[i][j] = [letter, bool];
+        var letter = this.state.board[i][j][0]
+
+        // If this is the first guess and it's correct, display
+        // If this is the first guess and it's wrong, reconceal
+        if (this.state.firstGuess == letter) {
+          newboard[i][j] = [letter, correctGuess];
+        }
+        //We know if we're here that the first guess doesn't match the second
+        // So conceal
+        else if (this.state.secondGuess == letter) {
+          newboard[i][j] = [letter, false];
         }
       }
     }
 
     var state1 = _.extend(this.state, {
       board: newboard,
-      activeGuess: ""
+      firstGuess: "",
+      secondGuess: "",
     });
     this.setState(state1);
   }
@@ -121,7 +167,15 @@ class MatchGame extends React.Component {
 
     if (cell[1] == true) {
       displayVal = cell[0];
-      return <td> {displayVal} </td>
+
+      if (this.state.firstGuess == displayVal ||
+        this.state.secondGuess == displayVal) {
+          return <td class="guessLetter"> {displayVal}</td>
+        }
+
+      else {
+        return <td> {displayVal} </td>
+        }
     }
 
     return <td>
@@ -147,19 +201,22 @@ class MatchGame extends React.Component {
       <p>Nothing here.</p>
     </div>;
 
-    for (var i = 0; i < 4; i++) {
-      for (var j = 0; j < 4; j++) {
-        console.log(this.state.board[i][j]);
-      }
-    }
 
-    return <table>
-      <tbody>
-      {this.renderBoardRow(0)}
-      {this.renderBoardRow(1)}
-      {this.renderBoardRow(2)}
-      {this.renderBoardRow(3)}
-      </tbody>
-    </table>
+
+    return <div>
+      <p>Your score is: {this.state.score}</p>
+
+      <table>
+        <tbody>
+          {this.renderBoardRow(0)}
+          {this.renderBoardRow(1)}
+          {this.renderBoardRow(2)}
+          {this.renderBoardRow(3)}
+        </tbody>
+      </table>
+
+    <button onClick={this.restart.bind(this)}>Restart</button>
+
+    </div>
   }
 }
